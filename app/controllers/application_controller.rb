2ec7f3e67ba6f3378f8ec9daf1bb693a5fbe74f0
Shaban Karumba & Teja kocjancic <skarumba@fcadmins-MacBook-Air.local>
2014-04-28 11:12:15 +0100

    added alternate layout page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      'not_signed_in'
+    else
+      'application'
+    end
+  end
 end
