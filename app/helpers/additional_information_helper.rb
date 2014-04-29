f74dd83ee0aa6e221da90690201bada8c5c9c2c6
Shaban Karumba & Teja kocjancic <skarumba@fcadmins-MacBook-Air.local>
2014-04-29 17:58:22 +0100

    Corrected morning typo

--- app/helpers/additional_information_helper.rb
+++ app/helpers/additional_information_helper.rb
@@ -1,6 +1,6 @@
 module AdditionalInformationHelper
   def prefered_times_list
-    [ "Early Morining (6-8am)", "Morining (9-11am)", "Mid Day (12-2pm)", "Daytime (3-6pm)", "Evening (7-10pm)"]
+    [ "Early Morning (6-8am)", "Morning (9-11am)", "Mid Day (12-2pm)", "Daytime (3-6pm)", "Evening (7-10pm)"]
   end
 
   def experience_types_list
--- spec/helpers/user_additional_information_helper_spec.rb
+++ spec/helpers/user_additional_information_helper_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe AdditionalInformationHelper do
   
   it "returns a list of prefered times" do
-    helper.prefered_times_list.should eq [ "Early Morining (6-8am)", "Morining (9-11am)", "Mid Day (12-2pm)", "Daytime (3-6pm)", "Evening (7-10pm)"]
+    helper.prefered_times_list.should eq [ "Early Morning (6-8am)", "Morning (9-11am)", "Mid Day (12-2pm)", "Daytime (3-6pm)", "Evening (7-10pm)"]
   end
 
   it "returns a list of experience types" do
