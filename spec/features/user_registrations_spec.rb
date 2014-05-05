1441f301e7dde84b1ae2eff727140cb1b96fe8dc
Shaban Karumba <shabankarumba@gmail.com>
2014-05-05 22:49:28 +0100

    Added another spec for missing data for the form

--- spec/features/user_registrations_spec.rb
+++ spec/features/user_registrations_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 feature "User Registration" do
   let(:user) { FactoryGirl.create(:user) }
-
+  let(:workout_preference) { FactoryGirl.create(:workout_preference) }
+ 
   scenario "sign up with corret data" do
     visit new_user_registration_path
     fill_in :user_first_name, with: user.first_name
@@ -24,8 +25,19 @@ feature "User Registration" do
     select "Beginner", from: :user_workout_preferences_experience
     select "Strength", from: :user_workout_preferences_exercise_type
     select "Early Morning (6-8am)", from: :user_workout_preferences_prefered_time
+    fill_in :user_workout_preferences_exercise_place, with: workout_preference.prefered_place
     click_button("Sign Up")
-    visit user_path
+    visit users_path
+    save_and_open_page
     page.should have_content("Thanks for signing up")
+  end
+
+  scenario "filling in additional information with missing data in some fields" do
+    visit edit_users_additional_path(user)
+    fill_in :user_about_me, with: user.about_me
+    select "Beginner", from: :user_workout_preferences_experience
+    click_button("Sign Up")
+    visit edit_users_additional_path(user)
+    page.should have_content("There was an error on the form")
   end
 end
