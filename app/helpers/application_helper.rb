8c0f01455adb8c0e0f9165955644dcb53ed69d1f
Shaban Karumba <shabankarumba@gmail.com>
2014-05-12 21:24:35 +0100

    Base64 encoding for the message query params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+require 'base64'
+
 module ApplicationHelper
+  def encode_user_params(user_id)
+    Base64.encode64("#{user_id}")
+  end
+
+  def decode_user_params(user_id)
+     Base64.decode64("#{user_id}")
+  end
 end
