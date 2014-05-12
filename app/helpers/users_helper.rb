7eac0b48b73702722c28db0642efccec4448bac4
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-12 10:49:42 +0100

    Renamed helper and removed duplicate option

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,13 @@
+module UsersHelper
+  def prefered_times_list
+    [ "Early Morning (6-8am)", "Morning (9-11am)", "Mid Day (12-2pm)", "Daytime (3-6pm)", "Evening (7-10pm)"]
+  end
+
+  def experience_types_list
+    ["Beginner", "Intermediate", "Advanced"]
+  end
+
+  def exercise_type_list
+    ["Strength", "Flexibility", "Endurance"]
+  end
+end
---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe UsersHelper do
+  
+  it "returns a list of prefered times" do
+    helper.prefered_times_list.should eq [ "Early Morning (6-8am)", "Morning (9-11am)", "Mid Day (12-2pm)", "Daytime (3-6pm)", "Evening (7-10pm)"]
+  end
+
+  it "returns a list of experience types" do
+    helper.experience_types_list.should eq [ "Beginner", "Intermediate", "Advanced" ]
+  end
+
+  it "returns a list of exercise types" do
+    helper.exercise_type_list.should eq ["Strength", "Flexibility", "Endurance"]
+  end
+end
