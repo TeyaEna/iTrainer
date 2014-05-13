4bcb3fe863b495d9580196bdf5c2c6f3f564bd68
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-13 14:32:02 +0100

    Removed base64 encoding for params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
-require 'base64'
-
 module ApplicationHelper
-  def encode_user_params(user_id)
-    Base64.encode64("#{user_id}")
-  end
-
-  def decode_user_params(user_id)
-     Base64.decode64("#{user_id}")
-  end
 end
