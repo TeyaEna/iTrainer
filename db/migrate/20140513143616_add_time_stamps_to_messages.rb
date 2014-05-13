4a4353fe43c5612984e4867bf2f8707c1a91c04c
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-13 15:39:59 +0100

    Added timestamp fields to messages table

---
+++ db/migrate/20140513143616_add_time_stamps_to_messages.rb
@@ -0,0 +1,5 @@
+class AddTimeStampsToMessages < ActiveRecord::Migration
+  def change
+    add_timestamps(:messages)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512133025) do
+ActiveRecord::Schema.define(version: 20140513143616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: true do |t|
-    t.integer "sender_id"
-    t.integer "receiver_id"
-    t.string  "subject"
-    t.text    "body"
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
