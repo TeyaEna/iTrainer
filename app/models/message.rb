95d8538a9dbd57f29e29cb1d10d1237d8f7e9c54
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-13 14:25:42 +0100

    Added a callback for getting the users screen name and also some validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,18 @@
 class Message < ActiveRecord::Base
+  attr_accessor :screen_name
   belongs_to :sender, class_name: 'User', foreign_key: "sender_id"
   belongs_to :receiver, class_name: 'User', foreign_key: "receiver_id"   
-  validates :body, :subject, presence: true   
+  validates :body, :subject, presence: true
+
+  before_save :set_receiver_id
+
+  def set_receiver_id
+    self.receiver_id = find_user_screen_name.id
+  end
+
+  private
+
+  def find_user_screen_name
+    User.find_by_screen_name(@screen_name)
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Message do
-  let(:message) { FactoryGirl.create(:message) }
+  subject { FactoryGirl.build(:message) }
+  let(:user_attr) { FactoryGirl.attributes_for(:user) }
 
   describe "validations" do
     it "a blank subject raises a validation error" do
@@ -12,6 +13,18 @@ describe Message do
     it "a blank body raises a validation error" do
       subject.body = ''
       subject.should have(1).error_on(:body)
+    end
+
+    context "screen name validation" do
+      before do
+        @user = User.create(user_attr)
+      end
+
+      it "sets the receiver_id based on the screen name of the receiver" do
+        subject.screen_name = "foobaz"
+        subject.save
+        subject.receiver_id.should eq(@user.id)  
+      end
     end
   end
 end
