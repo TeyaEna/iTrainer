78ce071c7a2bcc435546450e38fef8dd88469319
Shaban Karumba <shabankarumba@gmail.com>
2014-05-14 23:42:26 +0100

    Removed save open page debug

--- spec/features/message_inbox_spec.rb
+++ spec/features/message_inbox_spec.rb
@@ -4,16 +4,16 @@ feature "Message Inbox" do
   let(:user) { FactoryGirl.create(:user) }
 
   before do
-    login_as(user)
-    second_user = FactoryGirl.create(:second_user)
-    @message = FactoryGirl.create(:message, sender_id: second_user.id, receiver_id: user.id )
+    @user = FactoryGirl.create(:user)
+    login_as(@user)
+    @second_user = FactoryGirl.create(:second_user)
+    @message = FactoryGirl.create(:message, sender_id: @second_user.id, receiver_id: @user.id )
   end
 
   scenario "view message inbox" do
     visit root_path
     click_link("Inbox")
     visit inbox_index_path
-    save_and_open_page
     page.should have_content(@message.subject)
   end
 end
