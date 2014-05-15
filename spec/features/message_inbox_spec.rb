350e6827651921389f3163a25cea539903300931
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-15 17:55:24 +0100

    Corrected spelling mistake

--- spec/features/message_inbox_spec.rb
+++ spec/features/message_inbox_spec.rb
@@ -1,18 +1,34 @@
 require 'spec_helper'
 
 feature "Message Inbox" do
+
   let!(:user) { FactoryGirl.create(:user) }
   let!(:second_user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:second_user) { FactoryGirl.create(:user) }
 
   before do
     login_as(user)
     @message = FactoryGirl.create(:message, sender_id: second_user.id, receiver_id: user.id )
   end
 
-  scenario "view message inbox" do
+  context "when there are messages" do
+    before do 
+      @second_user = FactoryGirl.create(:user)
+      @message = FactoryGirl.create(:message, sender_id: @second_user.id, receiver_id: user.id )
+    end
+    scenario "view message inbox" do
+      visit root_path
+      click_link("Inbox")
+      visit inbox_index_path
+      page.should have_content(@message.subject)
+    end
+  end
+
+  scenario "when no messages are in the inbox" do
     visit root_path
     click_link("Inbox")
     visit inbox_index_path
-    page.should have_content(@message.subject)
-  end
+    page.should have_content("You have no messages in your inbox")
+  end 
 end
