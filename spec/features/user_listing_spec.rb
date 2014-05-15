eeb0da300cbf2200bca2752a16c53bee3e705c7f
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-15 18:09:44 +0100

    Using multiple users to test scenarios

--- spec/features/user_listing_spec.rb
+++ spec/features/user_listing_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
 
 feature "User listing" do
-  let(:user) { FactoryGirl.create(:user) }
+  let!(:current_user) { FactoryGirl.create(:user) }
+  let!(:lady_user) { FactoryGirl.create(:user, gender: "Female") }
+  let!(:young_user) { FactoryGirl.create(:user, age: 18) }
+  let!(:faraway_user) { FactoryGirl.create(:user, address: "N17 7NP") }
+  let!(:beginner_user) { FactoryGirl.create(:user, experience: "Beginner") }
+  let!(:strong_user) { FactoryGirl.create(:user, exercise_type: "Strength") }
 
   before do
-    login_as(user,scope: :user)
+    login_as(current_user,scope: :user)
   end
 
   context "as the current user" do
-    before do
-      @user_two = FactoryGirl.create(:second_user)
-    end
     scenario "list users besides the user" do
       visit users_path
-      page.should have_content(@user_two.screen_name)
-      page.should_not have_content(user.screen_name)
+      page.should have_content(lady_user.screen_name)
+      page.should have_content(young_user.screen_name)
+      page.should_not have_content(faraway_user.screen_name)
+      page.should_not have_content(current_user.screen_name)
     end
   
     context "searching based on criteria" do
@@ -23,8 +27,8 @@ feature "User listing" do
         select "Female", from: 'gender'
         click_button("Filter")
         visit searches_path
-        page.should have_content(@user_two.screen_name)
-        page.should_not have_content(user.screen_name)
+        page.should have_content(lady_user.screen_name)
+        page.should_not have_content(current_user.screen_name)
       end
 
       scenario "lists only users within an age range" do
@@ -32,7 +36,7 @@ feature "User listing" do
         select "18-35", from: 'age'
         click_button("Filter")
         visit searches_path
-        page.should have_content(@user_two.screen_name)
+        page.should have_content(young_user.screen_name)
       end
 
       scenario "lists only users with a given experience level" do
@@ -40,7 +44,7 @@ feature "User listing" do
         select "Beginner", from: 'experience'
         click_button("Filter")
         visit searches_path
-        page.should have_content(@user_two.screen_name)
+        page.should have_content(beginner_user.screen_name)
       end
 
       scenario "lists only users with a given exercise type" do
@@ -48,10 +52,11 @@ feature "User listing" do
         select "Strength", from: 'exercise'
         click_button("Filter")
         visit searches_path
-        page.should have_content(@user_two.screen_name)
+        page.should have_content(strong_user.screen_name)
       end
 
       scenario "list only users within an age range, gender, experience level and exercise type" do
+        ideal_match = FactoryGirl.create(:user, age: "18-35", gender: "Female")
         visit users_path
         select "18-35", from: 'age'
         select "Female", from: 'gender'
@@ -59,7 +64,7 @@ feature "User listing" do
         select "Strength", from: 'exercise'
         click_button("Filter")
         visit searches_path
-        page.should have_content(@user_two.screen_name)
+        page.should have_content(ideal_match.screen_name)
       end 
     end
   end
