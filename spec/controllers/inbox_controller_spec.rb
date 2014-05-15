71c84483dba31bf4a1ed10d722a1fde760992506
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-15 18:00:49 +0100

    Moved @ variables to let block

--- spec/controllers/inbox_controller_spec.rb
+++ spec/controllers/inbox_controller_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe InboxController do
 
+  let(:receiver) { FactoryGirl.create(:user) }
+  let(:sender) { FactoryGirl.create(:user) }
+  let!(:message) { FactoryGirl.create(:message, receiver: receiver, sender: sender)}
+
   before do
-    @user = FactoryGirl.create(:user)
-    sign_in(:user, @user)
-    @user_two = double("user_two", id: 2)
-    @message = FactoryGirl.create(:message, receiver_id: @user.id, sender_id: @user_two.id)
+    sign_in(:user, receiver)
   end
 
   describe "GET# index" do
@@ -17,7 +18,7 @@ describe InboxController do
 
     it "retrieves all the users messages" do
       get :index
-      assigns(:messages).should eq( { @user_two.id => [ @message ]} )
+      assigns(:messages).should eq( { sender.id => [ message ]} )
     end 
   end
 end
