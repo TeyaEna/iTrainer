b4808c41e8f80197d4f6685e90c7058ff77b497b
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-15 17:48:32 +0100

    Removed database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,28 +19,12 @@ RSpec.configure do |config|
   config.include Warden::Test::Helpers
   config.include Devise::TestHelpers, :type => :controller
   config.include FactoryGirl::Syntax::Methods
-  config.include Paperclip::Shoulda::Matchers
+  config.include Paperclip::Shoulda::Matchers #?
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
 
   Warden.test_mode!
-
-  config.before(:each) do
-    Warden.test_reset! 
-  end
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 
   config.include Capybara::DSL
 end
