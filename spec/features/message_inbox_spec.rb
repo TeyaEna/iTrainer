edc41774a69a3a269ceeac61e62e7b8d9fc08463
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-15 10:56:56 +0100

    Removed line

--- spec/features/message_inbox_spec.rb
+++ spec/features/message_inbox_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 feature "Message Inbox" do
-  let(:user) { FactoryGirl.create(:user) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:second_user) { FactoryGirl.create(:user) }
 
   before do
-    @user = FactoryGirl.create(:user)
-    login_as(@user)
-    @second_user = FactoryGirl.create(:second_user)
-    @message = FactoryGirl.create(:message, sender_id: @second_user.id, receiver_id: @user.id )
+    login_as(user)
+    @message = FactoryGirl.create(:message, sender_id: second_user.id, receiver_id: user.id )
   end
 
   scenario "view message inbox" do
