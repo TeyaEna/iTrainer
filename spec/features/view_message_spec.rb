1619f3c6754ffaf678544b231098f927aa30cc1f
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-15 18:10:38 +0100

    Removed @ variables

--- spec/features/view_message_spec.rb
+++ spec/features/view_message_spec.rb
@@ -2,19 +2,20 @@ require 'spec_helper'
 
 feature "Viewing a message" do
   let(:user) { FactoryGirl.create(:user) }
-  
+  let(:second_user) { FactoryGirl.create(:user) }
+  let!(:message) do 
+    FactoryGirl.create(:message, receiver_id: user.id, 
+      sender_id: second_user.id)
+  end
 
   before do
-    @user = FactoryGirl.create(:user)
     login_as(user)
-    @second_user = FactoryGirl.create(:second_user)
-    @message = FactoryGirl.create(:message, receiver_id: user.id, sender_id: @second_user)
   end
 
   scenario "by going clicking on a message link" do
     visit inbox_index_path
     click_link("Message")
-    visit message_path(@message.id)
-    page.should have_content(@message.body)
+    visit message_path(message.id)
+    page.should have_content(message.body)
   end
 end
