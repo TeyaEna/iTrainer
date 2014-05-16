8da2deededeb4f8063f1c7f18f9481f66d886b1e
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-16 15:31:59 +0100

    Changed the name of the scope slightly

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ActiveRecord::Base
   belongs_to :receiver, class_name: 'User', foreign_key: "receiver_id"   
   validates :body, :subject, presence: true
 
-  scope :received_messages, lambda { |user| where(receiver_id: user).order(created_at: :desc).group_by(&:sender_id)}
+  scope :all_received_messages, lambda { |user| where(receiver_id: user).order(created_at: :desc).group_by(&:sender_id)}
 
   scope :received_message, lambda {|receiver, sender| where(receiver_id: receiver, sender_id: sender).order(created_at: :desc) }
   scope :sent_message, lambda {|sender, receiver| where(sender_id: sender, receiver_id: receiver).order(created_at: :desc) }
