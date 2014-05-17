c88bec264fbe4e7d83bcc6d0273fbd5c361be7e6
Shaban Karumba <shabankarumba@gmail.com>
2014-05-17 12:05:49 +0100

    Moved instance variable from before block to let block

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe UsersController do
   let(:user_params) { FactoryGirl.attributes_for(:user) }
-  let(:user) { FactoryGirl.create(:user, address: "N16") }
+  let(:current_user) { FactoryGirl.create(:user, address: "N16") }
+  let(:user_one) { FactoryGirl.create(:user, address: "N16") }
+  let(:user_two) { FactoryGirl.create(:user, address: "N17 7NP") }
 
   before do
-    sign_in(:user, user)
+    sign_in(:user, current_user)
   end
 
   describe "GET #index" do
@@ -14,21 +16,16 @@ describe UsersController do
       response.should be_ok
     end
 
-
     context "listing users" do
-      before do
-        @user_one = FactoryGirl.create(:user, address: "N16")
-        @user_two = FactoryGirl.create(:user, address: "N17 7NP")
-      end
       context "all users except current user" do
         it "returns an array of users except the current user" do
           get :index
-          assigns(:users).should eq([ @user_one, @user_two ])
+          assigns(:users).should eq([ user_one, user_two ])
         end
 
         it "does not include the current user" do
           get :index
-          assigns(:users).should_not include(user)
+          assigns(:users).should_not include(current_user)
         end
       end
 
@@ -36,14 +33,14 @@ describe UsersController do
         it "orders the array of users by having the the closet in ascending order" do
           user_three = User.create(user_params)
           get :index
-          assigns(:users).should eq([ @user_one, @user_two ])
+          assigns(:users).should eq([ user_one, user_two ])
         end
       end
     end
 
     describe "GET show" do
       it "responds with a success" do 
-        get :show, { id: user.id }
+        get :show, { id: current_user.id }
         response.should be_ok
       end  
     end
