1cdb40c364f74dfafbdb172eb1dcbb49eed7c896
Shaban Karumba <shabankarumba@gmail.com>
2014-05-18 18:52:50 +0100

    Moved attributes into a constant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  PERMITTED_ATTRIBUTES = [:first_name, :last_name, :screen_name, :gender, :age,  :about_me, :address, :avatar, :latitude, :longitude, 
+  :exercise_type, :experience, :prefered_time, :prefered_place ].freeze
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   layout :layout_by_resource
@@ -13,12 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    [:first_name, :last_name, 
-      :screen_name, :gender, :age, 
-      :about_me, :address, 
-      :avatar, :latitude, :longitude, 
-      :exercise_type, :experience, 
-      :prefered_time, :prefered_place ].each do |attribute|
+    PERMITTED_ATTRIBUTES.each do |attribute|
       devise_parameter_sanitizer.for(:sign_up) << attribute
     end
   end
