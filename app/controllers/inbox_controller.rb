250354c7a69939e2e20e8797b12195c8e73de0b6
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-05-20 15:34:26 +0100

    Removes pagination as it's causing issues

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -2,6 +2,6 @@ class InboxController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @messages = Message.all_received_messages(current_user.id).page(params[:page]).per(10)
+    @messages = Message.all_received_messages(current_user.id)
   end
 end
